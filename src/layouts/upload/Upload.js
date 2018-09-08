a842c8d81d28827d2cad0de81fc471ae2a7747dd
Sharad Shekar <sharadshekar@gmail.com>
2018-09-08 02:17:16 -0500

    splitscreen UI, file preview, error display

--- src/layouts/upload/Upload.js
+++ src/layouts/upload/Upload.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
 import node from '../../util/ipfs.js';
+import default_preview from './file.png';
 
 const fileMaxSize = 9 * 1000000;
+const ipfsURL = 'https://ipfs.io/ipfs/';
 
 class UploadPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       filePath: '',
-      fileHash: ''
+      fileHash: '',
+      mimeType: '',
+      filePreview: '',
+      errors: []
     };
     this.addFile = this.addFile.bind(this);
     this.handleOnDrop = this.handleOnDrop.bind(this);
@@ -18,8 +23,19 @@ class UploadPage extends Component {
   handleOnDrop = (accepted, rejected, links) => {
     // console.log('Accepted: ', accepted[0]);
     // console.log('Rejected: ', rejected);
+    // Get file attributes
+    if (rejected.length !== 0) {
+      let errors = [];
+      errors.push('File Rejected: ', rejected[0].name);
+      return this.setState({ errors });
+    }
     const file = accepted[0];
     const fileName = file.name;
+    const mimeType = file.type;
+    const filePreview = file.preview;
+    this.setState({ mimeType, filePreview, errors: [] });
+
+    // Convert file from blob to buffer and ipfs add
     const reader = new FileReader();
     reader.readAsArrayBuffer(file);
     reader.onloadend = async () => {
@@ -29,6 +45,7 @@ class UploadPage extends Component {
   };
 
   addFile = async (buffer, fileName) => {
+    // ipfs add
     const filesAdded = await node.files.add({
       path: fileName,
       content: buffer
@@ -41,19 +58,51 @@ class UploadPage extends Component {
   };
 
   render() {
+    // Set content preview
+    let contentPreview;
+    switch (this.state.mimeType.split('/')[0]) {
+      case 'image':
+        contentPreview = <img src={this.state.filePreview} alt="file_image" />;
+        break;
+      case 'video':
+        contentPreview = (
+          <video muted autoplay controls src={this.state.filePreview}>
+            Sorry, your browser doesn't support embedded videos.
+          </video>
+        );
+        break;
+      default:
+        contentPreview = <img src={default_preview} alt="default_image" />;
+        break;
+    }
     return (
-      <main className="container">
-        <h1>Upload</h1>
-        <Dropzone
-          onDrop={this.handleOnDrop}
-          maxSize={fileMaxSize}
-          multiple={false}
-        >
-          {this.state.filePath ? this.state.filePath : 'Drop File here'}
-        </Dropzone>
-        <p>File hash: {this.state.fileHash}</p>
-        <p>File path: {this.state.filePath}</p>
-      </main>
+      <div className="container">
+        <div className="row">
+          <div className="split left">
+            <div className="centered">
+              <h1>Upload</h1>
+              <Dropzone
+                onDrop={this.handleOnDrop}
+                maxSize={fileMaxSize}
+                multiple={false}
+              >
+                {this.state.filePath ? contentPreview : null}
+                {this.state.filePath ? this.state.filePath : 'Drop File here'}
+              </Dropzone>
+              <br />
+              <p className="errors">
+                {this.state.errors ? this.state.errors.map(e => e) : null}
+              </p>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <p>File hash: {this.state.fileHash}</p>
+              <p>File path: {this.state.filePath}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
