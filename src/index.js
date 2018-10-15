e7bc5fd706d5993b5d5a13d665dcfbca543c83b1
Edmund Miller <edmund.a.miller@gmail.com>
2018-10-15 13:02:24 -0500

    Start using bootstrap css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Web3Provider } from 'react-web3';
