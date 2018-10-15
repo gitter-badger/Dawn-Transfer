515038010943146feca2ccc38a13bd1269f87352
Edmund Miller <edmund.a.miller@gmail.com>
2018-10-14 22:14:39 -0500

    Add bootstrap to index and App

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -7,7 +9,7 @@ import { Web3Provider } from 'react-web3';
 
 ReactDOM.render(
   // <Web3Provider>
-    <App />,
+  <App />,
   // </Web3Provider>,
   document.getElementById('root')
 );
