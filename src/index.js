2df32dae44fd431b4dbf4162c370e40d179a6b76
Edmund Miller <edmund.a.miller@protonmail.com>
2018-11-02 20:16:45 +0000

    Feature/jest Fix: #34

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import { Web3Provider } from 'react-web3';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Web3Provider } from 'react-web3';
 
 ReactDOM.render(
-  // <Web3Provider>
-  <App />,
-  // </Web3Provider>,
+  <Web3Provider>
+    <App />,
+  </Web3Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
