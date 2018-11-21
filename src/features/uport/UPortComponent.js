bc220fbf5afce3ee5c06a7e023a3a0d9fbbea45f
Edmund Miller <edmund.a.miller@gmail.com>
2018-11-21 15:00:35 -0600

    uPort now works with button login
    By default the user will be anonymous for now until I better understand
    uPort and how to log out. IE we know how to login and then cache that in
    their browser but we need to figure out how to log out.

--- src/features/uport/UPortComponent.js
+++ src/features/uport/UPortComponent.js
@@ -4,22 +4,34 @@ import PropTypes from 'prop-types';
 import { Connect } from 'uport-connect';
 const uport = new Connect('Aurora-test', { network: 'rinkeby' });
 
-uport.requestDisclosure({
-  requested: ['name', 'country'],
-  notifications: true,
-});
-uport.onResponse('disclosureReq').then(payload => {
-  const address = payload.address;
-});
-
 class UPortLogin extends Component {
   constructor(props) {
     super(props);
+    this.loginUPort = this.loginUPort.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  loginUPort() {
+    uport.requestDisclosure({
+      requested: ['name', 'country'],
+      notifications: true,
+    });
+
+    uport.onResponse('disclosureReq').then(payload => {
+      const address = payload.address;
+    });
   }
 
   render() {
     return (
-      <button type={'button'} className={'app-button primary'}>
+      <button
+        type={'button'}
+        className={'app-button primary'}
+        onClick={this.loginUPort}
+      >
         Login with uPort
       </button>
     );
